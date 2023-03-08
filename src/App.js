b4d1cee25c16306a49188e68e17bfdb91bdbfdf1
flh <Florian.Herzog@ti8m.ch>
2023-03-08 21:40:40 +0100

    hardcoded url

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import './App.css';
 
 function App() {
-  const redirectUrl = process.env['REDIRECT_URL'];
-  return redirectUrl?
-      <meta httpEquiv="Refresh" content="`0; url=process.env['REDIRECT_URL']" />
-      : <h1>ERROR: Environment Variable REDIRECT_URL missing.</h1>
+  return <meta httpEquiv="Refresh" content="`0; url='https://putaringonit-angular.oa.r.appspot.com'" />
 }
 
 export default App;
