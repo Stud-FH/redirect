426866ec8fd15bb5c1cc06cde300c7c8d76457f8
flh <Florian.Herzog@ti8m.ch>
2023-03-08 21:31:03 +0100

    redirecting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const redirectUrl = process.env['REDIRECT_URL'];
+  return redirectUrl?
+      <meta httpEquiv="Refresh" content="`0; url=process.env['REDIRECT_URL']" />
+      : <h1>ERROR: Environment Variable REDIRECT_URL missing.</h1>
 }
 
 export default App;
