c06377a5cb9efd57969c3dc4dac623eeb714533c
Florian <herzogflorian95@gmail.com>
2023-03-09 11:26:04 +0100

    debug

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const http = require('http');
 const fs = require('fs');
 const index = fs.readFileSync('index.html');
 
+console.log('environment variables:', process.env);
+
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(index);
