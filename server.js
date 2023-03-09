ff70f50a440594be4223aecef0860c13ec8b6667
Florian <herzogflorian95@gmail.com>
2023-03-09 14:45:37 +0100

    fixed

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 const http = require('http');
 
 http.createServer(function (req, res) {
-    if (!req.url.includes('error')) {
-        res.writeHead(500, {'Content-Type': 'text/plain'});
-        res.end('Error: env.REDIRECT_URL missing');
-    } else {
-        res.writeHead(302, {'Location': process.env['REDIRECT_URL'] ?? 'error'});
-        res.end();
-    }
+    res.writeHead(302, {'Location': process.env['REDIRECT_URL'] ?? 'https://putaringonit-angular.oa.r.appspot.com'});
+    res.end();
 }).listen(process.env['PORT'] ?? 8080);
